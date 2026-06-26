464b3c7fb41804ef1c336ba7d2802bd2969e1fb9
bitflipper19 <samyakpandey2007@gmail.com>
2026-06-26 17:29:59 +0530

    ALU

--- rtl/datapath/alu.v
+++ rtl/datapath/alu.v
@@ -1,13 +1,50 @@
-module lepusALU(
-    output [7:0] result,
-    output [1:0] flag,
-    input [7:0] operand_a, operand_b,
-    input [4:0] opcode,
+module LepusALU(
+    output reg [7:0] result,
+    output reg ZF,
+    output reg CF,
+    output reg BF,
+    input [7:0] a,
+    input [7:0] b,
+    input [4:0] opcode
 );
+
+    localparam ADD = 5'b01010,
+               SUB = 5'b01011,
+               XOR = 5'b01100,
+               OR  = 5'b01101,
+               AND = 5'b01110;
+
+    wire sub = (opcode == SUB);
+
+    wire [7:0] b_xor = b ^ {8{sub}};
+    wire [8:0] sum = a + b_xor + sub;
+
     always @(*) begin
-        case(op) 
-            // ALU Logic Here
+        result = 8'h00;
+        CF = 1'b0;
+        BF = 1'b0;
+        ZF = 1'b0;
+
+        case (opcode)
+
+            ADD: begin
+                result = sum[7:0];
+                CF = sum[8];
+            end
+
+            SUB: begin
+                result = sum[7:0];
+                BF = ~sum[8];
+            end
+
+            XOR: result = a ^ b;
+            OR : result = a | b;
+            AND: result = a & b;
+
+            default: result = 8'h00;
         endcase
+
+        ZF = (result == 8'h00);
     end
-    
+
 endmodule
