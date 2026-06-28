4e8a0f59c3bd3f8be6e1c512c48071af9855a842
bitflipper19 <samyakpandey2007@gmail.com>
2026-06-28 20:34:29 +0530

    minor fixes

--- rtl/datapath/tb/ir_tb.v
+++ rtl/datapath/tb/ir_tb.v
@@ -1,25 +1,24 @@
 module tb;
     wire [7:0] bus;
-    wire [4:0] opcode; wire [2:0] sub_opcode;
+    wire [7:0] opcode;
 
-    reg WR_1, WR_0, RD_0, clk, rst_n;
+    reg WR_HB, WR_LB, RD_LB, clk, rst_n;
     reg [7:0] driver;
 
-    assign bus = (WR_0|WR_1)? driver: 8'bz;
+    assign bus = (WR_LB|WR_HB)? driver: 8'bz;
 
     instruction_register DUT(
         .bus(bus),
         .opcode(opcode),
-        .sub_opcode(sub_opcode),
-        .WR_0(WR_0), .RD_0(RD_0),
-        .WR_1(WR_1), .clk(clk),
+        .WR_LB(WR_LB), .RD_LB(RD_LB),
+        .WR_HB(WR_HB), .clk(clk),
         .rst_n(rst_n)
     );
 
     initial begin
-        WR_1=1'b0; 
-        WR_0=1'b0;
-        RD_0=1'b0;
+        WR_HB=1'b0; 
+        WR_LB=1'b0;
+        RD_LB=1'b0;
         rst_n=1'b0;
         clk=1'b1;
         driver=8'hAC;
@@ -35,28 +34,28 @@ module tb;
     initial begin
         @(negedge clk)
         rst_n=1'b1;
-        WR_1=1'b1;
+        WR_HB=1'b1;
 
         @(negedge clk)
-        WR_1=1'b0;
+        WR_HB=1'b0;
 
         @(posedge clk)
         driver=8'h85;
 
         @(negedge clk)
-        WR_0=1'b1;
+        WR_LB=1'b1;
 
         @(negedge clk)
-        WR_0=1'b0;
+        WR_LB=1'b0;
 
         @(posedge clk)
         driver=8'h00;
 
         @(negedge clk) 
-        RD_0=1'b1;
+        RD_LB=1'b1;
 
         @(negedge clk)
-        RD_0=1'b0;
+        RD_LB=1'b0;
 
         #10 $finish;
     end
